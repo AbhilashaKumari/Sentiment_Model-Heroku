942cf71969b5c8a837ad6acdce2c9baf73066f42
Abhilasha <40860542+AbhilashaKumari@users.noreply.github.com>
2019-11-19 11:07:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+from form import MyForm
+from logging import FileHandler, WARNING
+import text_analysis
+
+app = Flask(__name__)
+
+file_handler = FileHandler('errorlog.txt') #file_handler logs errors
+file_handler.setLevel(WARNING)
+app.config.from_object(__name__) # Config for Flask-Session
+
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 #max upload size is 1MB
+app.config['SECRET_KEY'] = 'secret'
+app.config['ALLOWED_EXTENSIONS'] = set(['txt'])#restricts file extensions to the .txt extension
+app.config['SESSION_TYPE'] = 'filesystem' #config for Flask Session, indicates will store session data in a filesystem folder
+app.logger.addHandler(file_handler)
+Session(app) #create Session instance
+
+def allowed_file(filename):
+    '''Checks uploaded file to make sure it is.txt'''
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+   return render_template('home.html')
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        session['file_contents'] = form.name.data
+        return redirect('/analysis')
+    return render_template('submit.html', form=form)
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            session['file_contents'] = file.read().decode('utf-8')
+            return redirect(url_for('analysis'))
+    return render_template('upload.html')
+
+@app.route('/analysis')
+def analysis():
+    file_contents = session.get('file_contents')
+    analyzed_sent = text_analysis.get_pos_neg(file_contents)
+    pos_sent = analyzed_sent[-1]
+    neg_sent = analyzed_sent[0]
+    
+    word_cloud = text_analysis.render_word_cloud(file_contents)
+    return render_template('analysis.html', pos_sent=pos_sent, neg_sent=neg_sent, word_cloud=word_cloud)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+   app.run()
---
+++ text_analysis.py
@@ -0,0 +1,44 @@
+import base64
+from io import BytesIO
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import nltk
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from wordcloud import WordCloud
+
+
+def get_pos_neg(corpus):
+    '''Returns the most positive and negative sentences of the corpus.
+    '''
+    nltk.download('punkt')
+    sentences = nltk.sent_tokenize(corpus)
+    analyzer = SentimentIntensityAnalyzer()
+        #results=[]
+        #for sentence in sentences:
+        #result = analyzer.polarity_scores(sentence)
+        #print("result:" +str(result))
+        #results.append(result)
+
+    result = {sentence: analyzer.polarity_scores(sentence) for sentence in sentences}
+    sorted_result = sorted(result, key=lambda x: result[x]['compound'])
+    return [sorted_result[0], sorted_result[-1],result]
+
+
+
+def render_word_cloud(corpus):
+    '''Generates a word cloud using all the words in the corpus.
+    '''
+    fig_file = BytesIO()
+    
+    wordcloud = WordCloud(max_font_size=40, background_color='white').generate(corpus)
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig(fig_file, format='png')
+    fig_file.seek(0)
+    fig_data_png = fig_file.getvalue()
+    result = base64.b64encode(fig_data_png)
+    return result.decode('utf-8')
+
+
+
